650b8f84c41bfdb480d0287bbf6c579ba4b69146
Zillancer-Vartika66 <vartika.tiwari@zillancer.com>
2024-07-26 04:24:32 +0530

    personal-blog done

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5C0AR2fDHoadIT8WU5yTmF3kNoZPP1Ao",
+  authDomain: "blog-cd1ee.firebaseapp.com",
+  projectId: "blog-cd1ee",
+  storageBucket: "blog-cd1ee.appspot.com",
+  messagingSenderId: "454670555848",
+  appId: "1:454670555848:web:85c95f12bfd3b024dca3fd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
